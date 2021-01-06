bbbd0329b72bd2d53e873b15edada60407d5f925
Sriman Gaddam <srimangaddam@gmail.com>
2021-01-06 15:01:49 -0600

    Results Conditional Styling Added

--- components/ListResults.js
+++ components/ListResults.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import {View, Text, StyleSheet, Button, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
-
-const ListResults = ({item,navigate}) => {
+const ListResults = ({item, navigate}) => {
     //formats output from JSON output from barcode API
     const test = JSON
         .parse(item)
         .name
         .replace('""', ' ')
         .replace(/["]+/g, '')
-    return (
-        <TouchableOpacity style={styles.listItem}>
+    if (test==5) 
+        return (
+            <TouchableOpacity style={styles.listItem}>
+                <View style={styles.listItemView}>
+                    <Text style={styles.iconWarning}>
+                        <Icon name="warning" size={33}/></Text>
+                    <Text style={styles.listItemText}>{test}</Text>
+                    <View style={styles.btn}>
+                        <Button
+                            onPress={() => navigate(JSON.parse(item))}
+                            title="Info"
+                            color="#8b0000"/>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        )
+    else 
+        return (
+            <TouchableOpacity style={styles.listItem}>
             <View style={styles.listItemView}>
+                <Text style={styles.iconCheck}>
+                    <Icon name="check" size={33}/></Text>
                 <Text style={styles.listItemText}>{test}</Text>
                 <View style={styles.btn}>
-                <Button onPress={() => navigate(JSON.parse(item))} title="Info"/>
+                        <Button
+                            onPress={() => navigate(JSON.parse(item))}
+                            title="Info"
+                            color="#149414"/>
+                    </View>
                 </View>
-            </View>
-        </TouchableOpacity>
-    )
+            </TouchableOpacity>
+        )
 }
 
 const styles = StyleSheet.create({
     listItem: {
+        right: "1%",
         padding: "7%",
         borderBottomWidth: 1,
         borderColor: '#eee'
@@ -36,9 +58,21 @@ const styles = StyleSheet.create({
         fontSize: 18,
         width: "80%"
     },
-    btn:{
-        margin:5,  
-        flexDirection: 'row'
+    btn: {
+        flexDirection: 'row',
+        right: "2%"
+    },
+    iconWarning: {
+        right: "30%",
+        color: 'darkred',
+        fontSize: 20,
+        textAlign: 'center'
+    },
+    iconCheck: {
+        right: "30%",
+        color: 'green',
+        fontSize: 20,
+        textAlign: 'center'
     }
 })
 
